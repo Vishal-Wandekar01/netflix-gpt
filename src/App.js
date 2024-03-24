056bf2336199dd80ad80988aea1c3a83d77d08b3
Vishal Wandekar <varunwan@tibco.com>
2024-03-24 10:57:05 +0530

    Setup a routing in Body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="text-3xl font-bold text-green-400">
-      Namaste Everyone!!! Let's make a namaste Netflix GPT
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
